c7f0ae98bd977ddf0038aa0befe5b8e6c80f8552
Zaha250 <koltunov1998@mail.ru>
2022-05-18 17:28:58 +1000

    Чиста проекта

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import sum from './modules/sum';
-
-const rootEl = document.querySelector('#root');
-
-rootEl.textContent = sum(1, 11).toString();
--- src/modules/sum.js
+++
@@ -1,6 +0,0 @@
-export default function sum (...args) {
-    if(args.length < 1) {
-        throw Error('Функция должна принимать хотя бы 1 аргумент');
-    }
-    return args.reduce((acc, cur) => acc + cur, 0);
-}
